b5c858e5c5decfe1a33ef62e627447be07b92d25
Siddhartha Barman Joy <siddharthjoy88@gmail.com>
2022-03-03 03:05:05 +0600

    Create FiindCycleInDirectedGraph.cpp

---
+++ FiindCycleInDirectedGraph.cpp
@@ -0,0 +1,14 @@
+std::vector<ll> g[200005];
+int used[100005];
+
+int dfs(int v) {
+    used[v] = 1;
+    for (int to : g[v]) {
+        if (used[to] == 1) {
+            return 1;
+        }
+        if (!used[to] && dfs(to)) return 1;
+    }
+    used[v] = 2;
+    return 0;
+}
